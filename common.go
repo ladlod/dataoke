11e2d7bf8bcdf3b90d2ddc9a8fec79a4946eb807
ladlod <tian_zw@qq.com>
2021-06-03 16:33:10 +0800

    add method

--- common.go
+++ common.go
@@ -6,6 +6,13 @@ type CommonRespHeader struct {
 	Time int64  `json:"time"`
 }
 
+type ErrorResponse struct {
+	Msg     string `json:"msg"`
+	Code    int64  `json:"code"`
+	SubMsg  string `json:"sub_msg"`
+	SubCode int64  `json:"sub_code"`
+}
+
 type TaobaoGoodsSt struct {
 	// 商品相关信息
 	Id               int64    `json:"id"`               // 大淘客商品id
@@ -40,12 +47,12 @@ type TaobaoGoodsSt struct {
 	ShopLevel   int64          `json:"shopLevel"`   // 店铺等级
 
 	// 优惠券相关信息
-	CouponReceiveNum int64   `json:"couponReceiveNum"` // 领券量
 	CouponLink       string  `json:"couponLink"`       // 领券链接
 	CouponStartTime  string  `json:"couponStartTime"`  // 优惠券开始时间
 	CouponEndTime    string  `json:"couponEndTime"`    // 优惠券结束时间
 	CouponPrice      float64 `json:"couponPrice"`      // 优惠券金额
 	CouponConditions string  `json:"couponConditions"` // 优惠券最低消费
+	CouponReceiveNum int64   `json:"couponReceiveNum"` // 领券量
 	CouponTotalNum   int64   `json:"couponTotalNum"`   // 优惠券总数
 
 	// 活动相关信息
@@ -68,6 +75,62 @@ type TaobaoGoodsSt struct {
 }
 
 type TaoBaoOrderSt struct {
+	// 订单基本信息
+	TradeId          string           `json:"trade_id"`           // 订单编号
+	TradeParentId    string           `json:"trade_parent_id"`    // 买家在淘宝后台显示的订单编号
+	TkStatus         TaoBaoOrderState `json:"tk_status"`          // 订单状态
+	PayPrice         string           `json:"pay_price"`          // 买家确认收货的付款金额（不包含运费金额）
+	AlipayTotalPrice string           `json:"alipay_total_price"` // 买家拍下付款的金额（不包含运费金额）
+	DepositPrice     string           `json:"deposit_price"`      // 预售时期，用户对预售商品支付的定金金额
+	RefundTag        int64            `json:"refund_tag"`         // 维权标签 0.非维权 1.维权订单
+	OrderType        string           `json:"order_type"`         // 订单所属平台类型，包括天猫、淘宝、聚划算等
+	TerminalType     string           `json:"terminal_type"`      // 成交设备类型
+
+	// 时间信息
+	ClickTime     string `json:"click_time"`      // 点击时间
+	ModifiedTime  string `json:"modified_time"`   // 订单更新时间
+	TkCreateTime  string `json:"tk_create_time"`  // 订单创建的时间
+	TbPaidTime    string `json:"tb_paid_time"`    // 订单在淘宝拍下付款的时间
+	TbDepositTime string `json:"tb_deposit_time"` // 预售时期，用户对预售商品支付定金的付款时间
+	TkEarningTime string `json:"tk_earning_time"` // 订单确认收货后且商家完成佣金支付的时间
+
+	// 商品信息
+	ItemId           int64  `json:"item_id"`            // 商品id
+	ItemPrice        string `json:"item_price"`         // 商品单价
+	ItemImg          string `json:"item_img"`           // 商品主图
+	ItemTitle        string `json:"item_title"`         // 商品标题
+	ItemLink         string `json:"item_link"`          // 商品链接
+	ItemNum          int64  `json:"item_num"`           // 商品数量
+	ItemCategoryName string `json:"item_category_name"` // 商品所属的根类目，即一级类目的名称
+	FlowSource       string `json:"flow_source"`        // 产品类型
+	SellerNick       string `json:"seller_nick"`        // 掌柜旺旺
+	SellerShopTitle  string `json:"seller_shop_title"`  // 店铺名称
+
+	// 结算信息
+	TkOrderRole         int64  `json:"tk_order_role"`         // 2.佣金收益的第一归属者 3.从其他淘宝客佣金中进行分成的推广者
+	PubShareFee         string `json:"pub_share_fee"`         // 结算预估收入
+	PubShareRate        string `json:"pub_share_rate"`        // 从结算佣金中分得的收益比率
+	SubsidyRate         string `json:"subsidy_rate"`          // 平台给与的补贴比率
+	TkTotalRate         string `json:"tk_total_rate"`         // 提成=收入比率×分成比率。指实际获得收益的比率
+	AlimamaRate         string `json:"alimama_rate"`          // 推广者赚取佣金后支付给阿里妈妈的技术服务费用的比率
+	AlimamaShareFee     string `json:"alimama_share_fee"`     // 技术服务费=结算金额×收入比率×技术服务费率
+	SubsidyType         string `json:"subsidy_type"`          // 平台出资方，如天猫、淘宝、或聚划算等
+	SubsidyFee          string `json:"subsidy_fee"`           // 补贴金额=结算金额×补贴比率
+	TotalCommissionRate string `json:"total_commission_rate"` // 佣金比率
+	TotalCommissionFee  string `json:"total_commission_fee"`  // 佣金金额=结算金额＊佣金比率
+	IncomeRate          string `json:"income_rate"`           // 订单结算的佣金比率+平台的补贴比率
+	ShareRelativeRate   string `json:"share_relative_rate"`   // 专项服务费率
+	ShareFee            string `json:"share_fee"`             // 结算专项服务费
+
+	// 归因信息
+	AdzoneId   int64  `json:"adzone_id"`   // 推广位管理下的推广位名称对应的ID
+	PubId      int64  `json:"pub_id"`      // 推广者的会员id
+	SiteName   string `json:"site_name"`   // 媒体管理下的对应ID的自定义名称
+	AdzoneName string `json:"adzone_name"` // 推广位名称
+	SiteId     int64  `json:"site_id"`     // 媒体管理下的ID
+	SpecialId  int64  `json:"special_id"`  // 会员运营id
+	RelationId int64  `json:"relation_id"` // 渠道关系id
+	AppKey     string `json:"app_key"`     // 开发者调用api的appkey
 }
 
 // TaoBaoShopType 店铺类型
@@ -99,3 +162,18 @@ const (
 
 // TaoBaoOrderQueryType
 type TaoBaoOrderQueryType int64
+
+const (
+	QueryTypeCt = 1 // 订单创建时间
+	QueryTypePt = 2 // 订单支付时间
+	QueryTypeFt = 3 // 订单结算时间
+	QueryTypeUt = 4 // 订单更新时间
+)
+
+type TaoBaoOrderState int64
+
+const (
+	OrderStateTimeout = 1
+	OrderStateCancel  = 2
+	OrderStateRefund  = 3
+)
--- method.get.order.details.go
+++ method.get.order.details.go
@@ -1,16 +1,56 @@
 package dataoke
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-func (a *DaTaoKeApp) GetOrderDetails(queryType TaoBaoOrderQueryType, startTime int64, endTime int64, params Params) (err error) {
+type GetOrderDetailResp struct {
+	CommonRespHeader
+	Data *GetOrderDetailRespBody `json:"data"`
+}
+
+type GetOrderDetailRespBody struct {
+	HasPre        bool                    `json:"has_pre"`
+	HasNext       bool                    `json:"has_next"`
+	PageNo        int64                   `json:"page_no"`
+	PageSize      int64                   `json:"page_size"`
+	PositionIndex string                  `json:"position_index"`
+	Result        *GetOrderDetailResultSt `json:"result"`
+	ErrorResponse *ErrorResponse          `json:"error_response,omitempty"`
+}
+
+type GetOrderDetailResultSt struct {
+	PublisherOrderDto []*TaoBaoOrderSt `json:"publisher_order_dto"`
+}
+
+func (a *DaTaoKeApp) GetOrderDetails(queryType TaoBaoOrderQueryType, startTime int64, endTime int64, params Params) (res *GetOrderDetailRespBody, err error) {
 	params.Set("version", "v1.0.0")
 	params.Set("queryType", queryType)
 	params.Set("startTime", time.Unix(startTime, 0).Format("2006-01-02 15:04:05"))
 	params.Set("endTime", time.Unix(endTime, 0).Format("2006-01-02 15:04:05"))
-	_, err = a.postQuery(params, GetOrderDetailsURI)
+	bResp, err := a.postQuery(params, GetOrderDetailsURI)
 	if err != nil {
 		return
 	}
+	resp := &GetOrderDetailResp{}
+	err = dJson.Unmarshal(bResp, resp)
+	if err != nil {
+		return
+	}
+	if resp.Code != 0 {
+		err = fmt.Errorf("%v", resp.Msg)
+		return
+	}
+	if resp.Data == nil {
+		err = fmt.Errorf("nil resp")
+		return
+	}
+	if resp.Data.ErrorResponse != nil {
+		err = fmt.Errorf("%v", resp.Data.ErrorResponse.SubMsg)
+		return
+	}
+	res = resp.Data
 
 	return
 }
